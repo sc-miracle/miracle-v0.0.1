a1fc4840b8117920f6919546d2b9688d5233d64e
sc-miracle <plus_chou@foxmail.com>
2025-11-21 12:06:51 +0000

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'midas'
-copyright = '2024, labomics'
-author = 'labomics'
-release = '0.1.13'
+project = 'miracle'
+copyright = '2025, xxx'
+author = 'xxx'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
